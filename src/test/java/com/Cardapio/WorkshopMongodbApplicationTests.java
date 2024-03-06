0af3bcd498cb1c2e8a7a0f64d0b36f6900669419
João Vitor Carvalho <100238388+JoaovitorCarvalho20@users.noreply.github.com>
2024-03-06 19:37:15 -0300

    Primeiro commit

---
+++ src/test/java/com/Cardapio/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
